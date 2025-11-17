e148fc67e9989644f644a0254fc1ae5573c1a16b
rabituwu <jm.tibar14@gmail.com>
2025-11-17 21:36:01 +0800

    more css

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>@yield("title", "PRINTING_SHOP")</title>
+    <title>@yield("title", "Secret Shop")</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
   <link href="/css/status.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,29 +1,31 @@
 @extends('layouts.guest')
 
 @section('content')
-<div class="main-page d-flex justify-content-center align-items-center min-vh-100">
-    <div class="container text-center">
-        <h1 class="mb-5">Welcome to the Dashboard</h1>
+<div class="main-page-container">
+    <div class="main-page">
+        <div class="container text-center">
+            <h1 class="mb-5">Welcome to the Secret Shop!</h1>
 
-        <div class="row justify-content-center">
-            <!-- Printing -->
-            <div class="col-md-3 mb-4">
-                <a href="{{ route('printing') }}" class="text-decoration-none">
-                    <div class="card p-4 shadow-sm section-card">
-                        <h3 class="text-primary">Printing</h3>
-                        <p class="text-muted">Start recording printing items here.</p>
-                    </div>
-                </a>
-            </div>
+            <div class="row justify-content-center">
+                <!-- Printing -->
+                <div class="opts col-md-5 mb-4">
+                    <a href="{{ route('printing') }}" class="text-decoration-none">
+                        <div class="card p-4 section-card">
+                            <h3><i class="fa-solid fa-print fa-xs"></i> Printing <i class="fa-solid fa-print fa-xs"></i></h3>
+                            <p>Start recording printing items.</p>
+                        </div>
+                    </a>
+                </div>
 
-            <!-- Repair -->
-            <div class="col-md-3 mb-4">
-                <a href="{{ route('repair') }}" class="text-decoration-none">
-                    <div class="card p-4 shadow-sm section-card">
-                        <h3 class="text-primary">Repair</h3>
-                        <p class="text-muted">Start recording repair items here.</p>
-                    </div>
-                </a>
+                <!-- Repair -->
+                <div class="opts col-md-5 mb-4">
+                    <a href="{{ route('repair') }}" class="text-decoration-none">
+                        <div class="card p-4 section-card">
+                            <h3><i class="fa-solid fa-screwdriver-wrench fa-xs"></i> Repair <i class="fa-solid fa-screwdriver-wrench fa-xs"></i></h3>
+                            <p>Start recording repair items.</p>
+                        </div>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
